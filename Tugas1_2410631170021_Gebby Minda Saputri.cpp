3ae7c1e054a3edaa470f8e323a3fb12d0bd8d6d2
GebbyMS <abdul.amin0292@gmail.com>
2024-09-16 18:42:00 +0700

    TUGAS ALGORITMA PERTEMUAN 1
    Nama: Gebby Minda Saputri
    NPM: 2410631170021
    Kelas: 1E Informatika

---
+++ Tugas1_2410631170021_Gebby Minda Saputri.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+int main ()
+{
+    std::string Nama;
+    std::cout << "Nama : ";
+    std::cout << "Gebby Minda Saputri " << Nama << std::endl;
+    std::string NPM;
+    std::cout << "NPM : ";
+    std::cout << "2410631170021" << NPM << std::endl;
+    std::string Kelas;
+    std::cout << "Kelas : ";
+    std::cout << "1E Informatika" << Kelas << std::endl;
+    std::string Citacita;
+    std::cout << "Cita-cita : ";
+    std::cout << "Menjadi WNA, orang kaya gaji 5 digit, programmer(AAMIIN BGTT)" << Citacita << std::endl;
+    std::string MotivasimasukInformatika;
+    std::cout << "Motivasi masuk jurusan Informatika : ";
+    std::cout << "Peluang karir yang luas dan gaji yag kompetitif serta dikarenakan teknologi semakin canggih yang membuat saya tertarik masuk kedalam IT dan juga supaya saya tidak ketinggalan zaman." << MotivasimasukInformatika << std::endl;
+    std::string Harapan;
+    std::cout << "Harapan : ";
+    std::cout << "Semoga cita-cita tersebut bisa tercapai dan semoga mendapat IPK 4.00 serta bisa study abroad/MSIB melalui MBKM dan menjadi ketua di salah satu ormawa." << Harapan << std::endl;
+    std::cin >> Nama, Kelas, NPM, Citacita, MotivasimasukInformatika, Harapan;
+    return 0;
+
+}
